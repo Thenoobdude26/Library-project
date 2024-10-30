e256b5ac0b1ab2b6758f75f61a3241b542a7fcfd
Dogge <tatendacoolboy@gmail.com>
2024-10-30 17:41:16 +0800

    Library edits

--- T's Code bin.py
+++ T's Code bin.py
@@ -1,61 +1,4 @@
 import datetime
-# #super admin username: SDM001 and password: SDMpassword
-# # Log in as super admin first to create an admin account
-# #step 1: log in as super admin (admin account that existed after running the program)
-# #step 2: add a new admin account for new employees (Make sure the username start with ADM) ex. ADM002 ADM001 
-# #step 3: log in as the admin account you created
-# #step 4: you are now able to unlock admin perms
-# #Only Super Admin can add staff, because yes
-
-
-
-# def Addstaff():
-#     dik = {"STAFF": []}
-#     name_staff = input('Insert Name: ')
-#     id_staff = input('Insert ID: (ADM-ADMIN, LB-librarian)')#ADM001/LB0001
-#     email_staff = input('Insert Email: ')
-#     password_staff = input('Insert: ')
-#     AdminPerms = input('Has Admins: ')#Librerians don't have admin status
-#     new_staff = {'Name': name_staff, 'ID': id_staff, 'Email': email_staff, 'HasPerms': AdminPerms}
-#     encryption = password_staff.encode("utf-8").hex()
-#     passwords = {'Password': encryption}  # adding password encryption
-#     dik["STAFF"].append(new_staff)
-#     dik["STAFF"].append(passwords)#Password stored in hex
-#     with open('staff_list.txt', "a") as t:
-#         t.write(str(dik) + '\n')
-#     print(f'Added: {name_staff}')
-
-# def Addmember():
-#     dic = {"MEMBER": []}
-#     name_member = input('Insert Name: ')
-#     id_member = input('Insert ID: (M###)')#M001
-#     email_member = input('Insert Email: ')
-#     password_member = input('Insert: ')
-#     new_member = {'Name': name_member, 'ID': id_member, 'Email': email_member}
-#     encryption = password_member.encode("utf-8").hex()
-#     passwords = {'Password': encryption}  # adding password encryption
-#     dic["MEMBER"].append(new_member)
-#     dic["MEMBER"].append(passwords)#Password stored in hex
-#     with open('member_list.txt', "a") as t:
-#         t.write(str(dic) + '\n')
-#     print(f'Added: {name_member}')
-    
-# # Addmember()
-# # Addstaff()
-
-# #     dik["FoodItem"].append(newfood)  # Append the food dictionary to 'FoodItem' list
-# #     with open('food.txt', "a") as t:  # Use 'with' to handle file opening and closing
-# #         t.write(str(dik) + '\n')
-# #     print(f'Added: {food_name}')
-
-
-# # signin = {"staff_name":staffname,
-# #      "staffId": staffId,
-# #      "staff_email": staffemail,
-# #      "staffPass":staffPass,
-# #      "hasAdminperms":Admin
-# #     }
-
 #Work in progress
 def AddBook():
     print("Insert Book details Below: ")
@@ -151,4 +94,62 @@ def return_book():
 
     with open('loan_logs.txt', 'a') as log_file:
         log_file.write(f"ISBN: {isbn}, Member ID: {returner}, Date Returned: {datereturned}\n")
-# AddBook()
+
+def view_loan_logs():
+    with open('loan_logs.txt', 'r') as file:
+        for line in file:
+            print(line)
+
+def search_loan():
+    print("Insert ID: ")
+    memberid = input(" ")
+    loans = []
+
+    with open('loan_logs.txt', 'r') as file:
+        for line in file:
+            if memberid.lower() in line.lower():
+                loans.append(line.strip())
+
+    if not loans:
+        print("No Loans found for user.")
+        return
+
+    # Find the most recent loan
+    most_recent_loan = None
+    for loan in loans:
+        if "Date Loaned" in loan:
+            most_recent_loan = loan
+
+    if most_recent_loan:
+        print(most_recent_loan)
+        loan_date_str = most_recent_loan.split("Date Loaned: ")[1]
+        loan_date = datetime.datetime.strptime(loan_date_str, "%Y/%m/%d").date()
+        current_date = datetime.datetime.now().date()
+        days_since_loan = (current_date - loan_date).days
+
+        # Check if the loan has been returned
+        if "Date Returned" not in most_recent_loan:
+            days_left = 14 - days_since_loan
+            if days_left > 0:
+                print(f"Time left before fee: {days_left} days")
+            else:
+                days_late = abs(days_left)
+                if days_late == 1:
+                    fee = 2
+                elif days_late == 2:
+                    fee = 3
+                elif days_late == 3:
+                    fee = 4
+                elif days_late == 4:
+                    fee = 5
+                elif days_late == 5:
+                    fee = 6
+                else:
+                    fee = 10
+                print(f"Late fee: {fee} RM")
+        else:
+            print("No late fee.")
+    else:
+        print("No recent loans found.")
+
+
